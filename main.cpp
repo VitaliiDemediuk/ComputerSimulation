541689c901eb088eef2fa2367fec95a0c62fa2ca
Vitalii Demediuk <demedyuk.v.i@gmail.com>
2021-11-25 16:08:46 +0200

    read data.

--- main.cpp
+++ main.cpp
@@ -16,6 +16,14 @@ namespace {
 
 int main() {
     using namespace ComSim;
-
+    const auto n = read_param<size_t>("Antenna numbers = ");
+    std::vector<AntennaData> data;
+    data.reserve(n);
+    for (size_t i = 0; i < n; ++i) {
+        const auto x = read_param<size_t>("x = ");
+        const auto y = read_param<size_t>("y = ");
+        const auto alpha = read_param<size_t>("alpha = ");
+        data.emplace_back(x, y, alpha);
+    }
     return 0;
 }
